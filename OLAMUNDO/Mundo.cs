e57b386f276e21a18d533bb644dd1057ad5294f9
MarcielRocha <marcielrochapj@gmail.com>
2023-06-04 09:14:28 -0300

    Merge branch 'main' of https://github.com/MarcielRocha/VS_Trabalho2

--- OLAMUNDO/Mundo.cs
+++ OLAMUNDO/Mundo.cs
@@ -7,11 +7,18 @@ public class Mundo
     {
         var terra = new OLAMUNDO.Mundo();
         terra.Mensagem("Olá mundo da Terra");
+        terra.Conta(560,780);
 
     }
 
     public void Mensagem(string msg)
     {
         Console.WriteLine(msg);
+    }
+
+    public void Conta(int a, int b)
+    {
+        int c = a * b;
+        Console.WriteLine(c);
     }
 }
